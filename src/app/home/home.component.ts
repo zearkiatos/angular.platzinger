7e16f6f5406c68b0fc642c13c69628beeb2c29e3
Pedro Capriles <caprilespe@outlook.com>
2019-10-21 20:41:13 -0300

    NgFor applied in the users lists

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { IUser } from '../interfaces/IUser';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-
+  public friends: IUser[];
   constructor() {
     
     let myUser:IUser = {
@@ -19,9 +19,57 @@ export class HomeComponent implements OnInit {
       uid:1
     };
 
+    let user1:IUser = {
+      nick:'Pedro',
+      subnick: 'Pedro',
+      age:60,
+      email:'pedro@outlook.com',
+      friend: true,
+      uid:1
+    };
+
+    let user2:IUser = {
+      nick:'Roberto',
+      subnick: 'Roberto',
+      age:30,
+      email:'roberto@outlook.com',
+      friend: true,
+      uid:1
+    };
+
+    let user3:IUser = {
+      nick:'Yessenia',
+      subnick: 'Yessenia',
+      age:33,
+      email:'Yessenia@outlook.com',
+      friend: true,
+      uid:1
+    };
+
+    let user4:IUser = {
+      nick:'Maria',
+      subnick: 'Maria',
+      age:30,
+      email:'maria@outlook.com',
+      friend: true,
+      uid:1
+    };
+
+    let user5:IUser = {
+      nick:'Diana',
+      subnick: 'Diana',
+      age:12,
+      email:'diana@outlook.com',
+      friend: true,
+      uid:1
+    };
+
+
     let users:IUser[] = [
       myUser
     ];
+
+    this.friends = [user1,user2,user3,user4,user5];
 
     console.log(myUser);
 
