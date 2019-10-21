c90211534f603c44dd06b267db4aea71b02d93e3
Pedro Capriles <caprilespe@outlook.com>
2019-10-21 17:13:22 -0300

    How to use data type typescript

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    let c:number = 1;
+    let b:number = 2;
+
+    let e:string = '1';
+    let f:string = '2';
+    console.log(e+f);
+
+    let g:boolean = true;
+    let h:object = {};
+
+    console.log(g);
+
+    
+    let i = [c, b, e, f, g, h];
+
+    console.log(i);
+
+    let j:boolean[] = [true, g];
+
+    let k: object[] = [{},h];
+
+    let l:any[]= [1, 'aoe',{},[]];
+
+  }
 
   ngOnInit() {
   }
