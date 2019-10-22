16fc5bc48281196fe81de8f65fce77e8b1f32e0d
Pedro Capriles <caprilespe@outlook.com>
2019-10-22 15:43:31 -0300

    How to use css styles and reference resources

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
