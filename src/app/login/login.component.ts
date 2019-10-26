caa3112a242296cbec53b5312e6d47e26c8dc12a
Pedro Capriles <caprilespe@outlook.com>
2019-10-26 19:35:09 -0300

    Login/Register of users with email and password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public operation:string="login";
-  constructor() { }
+  public operation: string = "login";
+  public model = {
+    email: "",
+    password: ""
+  }
+  constructor(private authenticationService: AuthenticationService) {
+
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authenticationService.loginWithEmail(this.model.email, this.model.password).then((data) => {
+      alert("Loggeado correctamente");
+      console.log(data);
+    }).catch((error) => {
+      alert("Ocurrio un error");
+      console.log(error);
+    });
+  }
+
+  loginWithFacebook() {
+    this.authenticationService.loginWithFacebook().then((data)=>{
+      alert("Loggeado correctamente");
+      console.log(data);
+    }).catch((error)=>{
+      alert("Ocurrio un error");
+      console.log(error);
+    });
+  }
+
+  register() {
+    this.authenticationService.registerWithEmail(this.model.email, this.model.password).then((data) => {
+      alert("Registrado correctamente");
+      console.log(data);
+    }).catch((error) => {
+      alert("Ocurrio un error");
+      console.log(error);
+    });
   }
 
 }
