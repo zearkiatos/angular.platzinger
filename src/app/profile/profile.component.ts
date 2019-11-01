fc38c8bd2adc8bb38f78499cb4317d09ef0fa50e
Pedro Capriles <caprilespe@outlook.com>
2019-11-01 00:59:14 -0300

    Saving the profile images in our firebase data base

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,16 +3,18 @@ import { IUser } from '../interfaces/IUser';
 import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { AngularFireStorage } from '@angular/fire/storage';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  public user:IUser;
+  public user: IUser;
   imageChangedEvent: any = '';
   croppedImage: any = '';
-  constructor(private userService: UserService, private authenticationService: AuthenticationService) {
+  picture:any;
+  constructor(private userService: UserService, private authenticationService: AuthenticationService, private firebaseStorage: AngularFireStorage){
     this.authenticationService.getStatus().subscribe((status) => {
       this.userService.getUserById(status.uid).valueChanges().subscribe((data: IUser) => {
         this.user = data;
@@ -29,12 +31,33 @@ export class ProfileComponent implements OnInit {
   }
 
   saveSettings() {
-    this.userService.editUser(this.user).then(() => {
-      alert("Cambios Guardados");
-    }).catch((error) => {
-      alert("Hubo un error");
-      console.log(error);
-    });
+    if (this.croppedImage) {
+      const currentPictureId = Date.now();
+      const pictures = this.firebaseStorage.ref('pictures/'+currentPictureId+'.jpg').putString(this.croppedImage,'data_url');
+
+      pictures.then((data)=>{
+        this.picture = this.firebaseStorage.ref("pictures/"+currentPictureId+'.jpg').getDownloadURL();
+        this.picture.subscribe((p)=>{
+          this.userService.setAvatar(p, this.user.uid).then(()=>{
+            alert("Avatar subido correctamente");
+          }).catch((error)=>{
+            alert("Hubo un error al tratar de subir la imagen.");
+            console.log(error);
+          });
+        });
+      }).catch((error)=>{
+        console.log(error);
+      });
+    }
+    else {
+      this.userService.editUser(this.user).then(() => {
+        alert("Cambios Guardados");
+      }).catch((error) => {
+        alert("Hubo un error");
+        console.log(error);
+      });
+    }
+
   }
 
   fileChangeEvent(event: any): void {
